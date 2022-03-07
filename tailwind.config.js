52f9dec80440e38dae99cd19589a7c6272e822a4
Arnav Gosain <arnav@arnavgosain.com>
2022-03-07 09:29:14 +0530

    fix: fix tailwind content match

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ["./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				...colors,
+			},
+		},
+	},
+	plugins: [],
+};
