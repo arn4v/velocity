d38bc86318dd3753d164938c78441f414a94f8f8
Arnav Gosain <arnav@arnavgosain.com>
2022-03-07 09:23:22 +0530

    chore: project-wide format

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { withTRPC } from '@trpc/next';
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { withTRPC } from "@trpc/next";
 import { httpLink } from "@trpc/client/links/httpLink";
-import superjson from 'superjson'
-import type { AppRouter } from '~/server/routers/_app';
-import { getDeploymentUrl } from '~/lib/utils.all';
-
+import superjson from "superjson";
+import type { AppRouter } from "~/server/routers/_app";
+import { getDeploymentUrl } from "~/lib/utils.all";
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return <Component {...pageProps} />;
 }
 
 export default withTRPC<AppRouter>({
--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -23,4 +23,4 @@ export const appRouter = createRouter()
 		}),
 	);
 
-	export type AppRouter = typeof appRouter
+export type AppRouter = typeof appRouter;
